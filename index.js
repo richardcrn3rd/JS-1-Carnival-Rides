b6907b7784dd902f843e68fb19abc3c3496f99b6
“richardcrn3rd” <richardcrn3rd@gmail.com>
2024-03-02 00:06:21 -0500

    Added If Statements

--- index.js
+++ index.js
@@ -11,21 +11,30 @@ let waterIsOn = true
 
 //Ride 1 works when:
     // green wire is connected
-ride1PowerOn = false
 
+if (greenWireConnected){
+    ride1PowerOn = true
+}
 //Ride 2 works when 
     //green and yellow wires are connected
-ride2PowerOn = false
 
+if (greenWireConnected && yellowWireConnected){
+ride2PowerOn = true
+}
 //Ride 3 works when:
     // green and red wires are connected
     // water is on 
     // yellow wire is not connected
-ride3PowerOn = false
 
+if (greenWireConnected && redWireConnected && waterIsOn || !yellowWireConnected){
+ride3PowerOn = true
+}
 //Ride 4 has power when:
     //ride wire is connected
-ride4PowerOn = false
+
+if (redWireConnected){
+ride4PowerOn = true
+}
 //Ride 4 is a slot machine
     //if a 1 is rolled it should return "star"
     //if a 2 is rolled it should return "car"
@@ -36,4 +45,24 @@ ride4PowerOn = false
     //if a 7 is rolled it should return "seven"
 function ride4SlotMachine(rolledNumber){
     return "nothing"
+
+    switch (rolledNumber) 
+    {
+        case 1:
+            return "star"
+        case 2:
+            return "car"
+        case 3:
+            return "pot of gold"
+        case 4:
+            return "clover"
+        case 5:
+            return "bowling ball"
+        case 6:
+            return "tiger"
+        case 7:
+            return "seven"
+        default:
+            break;
+    }
 }
